de019217daabd0ccbb436945e7668bdd6a01755d
Jenny Gardner <j.rongjie@gmail.com>
2020-02-22 12:35:50 -0500

    Cole's amazing and this works

--- harborside-test/wordChanger.js
+++ harborside-test/wordChanger.js
@@ -1,34 +1,16 @@
-//reference https://javascript.info/array-methods
+//Need to create a loop in which every 1.5 seconds the word changes to a new one going down the array next word. Needs to repeat forever
 
-const card = document.getElementById("card");
-let word = document.getElementById("word");
-let nextWord = ["Joy", "Community", "Peace", "Strength", "Fullness", "Victory", "Healing", "Purpose", "Rest", "Wisdom", "Freedom"]; //This is the list of word that will be cycled through, if you change it make sure that it's in quotes and has a comma between each word
+//https://repl.it/@ColeHenson/help-jenny
 
-const speed = 2000; //time is in miliseconds
+const words = ["Joy", "Community", "Peace", "Strength", "Fullness", "Victory", "Healing", "Purpose", "Rest", "Wisdom", "Freedom"]; //This is the list of word that will be cycled through, if you change it make sure that it's in quotes and has a comma between each word
+const speed = 1500; //time is in miliseconds
+let wordReplacement = document.querySelector("#word");
+let currentWord = 0;
 
 //acts as a timer
-setInterval(function(){
-    //acts as a loop to go through array positions
-    //replaces word with next word
-        //replaceChildNode
-            //card.replaceChild(nextWord, word);
-
-}, speed);
-
-/*
-const body = document.body;
-const six = body.innerHTML;
-loopThroughArray(nextWord, function (arrayElement, loopTime) {
-    six += arrayElement+ ": " + loopTime+ "<br/>";
-}, 1000);
-
-function loopThroughArray (array, callback, interval) {
-    let newLoopTimer = new LoopTimer(function (time) {
-        var element = array.shift();
-        callback(element, time - start);
-        array.push(element);
-    }, interval);
-
-    let start = newLoopTimer.start();
-};
-*/
+window.setInterval(function replaceWord() {
+    currentWord++;
+    
+    if (currentWord > words.length-1) currentWord = 0;
+    wordReplacement.innerText = words[currentWord];
+  }, speed);
